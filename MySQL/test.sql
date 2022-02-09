1f4b5c562c18e12ca919a43d0ced5cc3aafc10e7
Bjørnar Borge <72221999+bjurneiz@users.noreply.github.com>
2022-02-09 20:10:35 +0100

    Regisrer.php updated

--- MySQL/test.sql
+++ MySQL/test.sql
@@ -1,4 +1,14 @@
 CREATE DATABASE financeBudgetApp;
 
+USE financeBudgetApp;
+
 CREATE TABLE customer (
+	CustomerID int NOT NULL,
+    Username varchar(50) NOT NULL,
+    EMail varchar(100) NOT NULL,
+    Password varchar(100) NOT NULL
 );
+
+SELECT *CustomerID
+FROM customer;
+
--- registrer.php
+++ registrer.php
@@ -1,18 +1,21 @@
 <?php
+$host = 'finance-budget-app.mysql.database.azure.com';
+$username = 'FinanceBudgetApp';
+$password = 'Gruppe654321';
+$db_name = 'financebudgetapp';
 
-$conn = mysqli_connect('finance-budget-app.database.windows.net','leder','Gruppe654321','financebudgetapp');
-
-if(!$conn)
-{
-	die('Connection failed!'.mysqli_error($conn));
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$sql = "INSERT INTO Customer(Id, Username, EMail, Password) VALUES(1, '$username', '$email','$password',
- '$pwd')";
+$sql = "INSERT INTO Customer(Id, Username, EMail, Password) VALUES(1, '$username', '$email','$password')";
 
 if(mysqli_query($conn,$sql))
 {
