62eb391867e458899a7bba22b9ec17447a873fcb
Bjørnar Borge <72221999+bjurneiz@users.noreply.github.com>
2022-02-16 11:06:16 +0100

    Liten endring på register

--- Register/register.php
+++ Register/register.php
@@ -1,15 +1,17 @@
 <?php
 include '../connect_mysql/connect.php';
 $conn = OpenCon();
+echo "Connect Successfully";
+echo "'\n";
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 // Run the create table query
-$sql = "INSERT INTO customer(CustomerID, EMail, Password) VALUES('5', '$email','$password')";
+$sql = "INSERT INTO customer(CustomerID, EMail, Password) VALUES('2', '$email','$password')";
 
  if(mysqli_query($conn,$sql)) {
-	 header("location:../index.html");
+	 echo "Registerd Successfully";
  } else {
 	 echo mysqli_error($conn);
 }
