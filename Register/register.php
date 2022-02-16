993b70f317abd3cfb927f4257a5f89792b108059
Bjørnar Borge <72221999+bjurneiz@users.noreply.github.com>
2022-02-16 09:58:04 +0100

    Fikset så du kommer tilbake til index når du har registrert bruker

--- Register/register.php
+++ Register/register.php
@@ -1,17 +1,15 @@
 <?php
 include '../connect_mysql/connect.php';
 $conn = OpenCon();
-echo "Connect Successfully";
-echo "'\n";
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 // Run the create table query
-$sql = "INSERT INTO customer(CustomerID, EMail, Password) VALUES('2', '$email','$password')";
+$sql = "INSERT INTO customer(CustomerID, EMail, Password) VALUES('5', '$email','$password')";
 
  if(mysqli_query($conn,$sql)) {
-	 echo "Registerd Successfully";
+	 header("location:../index.html");
  } else {
 	 echo mysqli_error($conn);
 }
