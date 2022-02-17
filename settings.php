cfe9e497ca8ff49b97a779a711b9b90a1c9b614d
Bjørnar Borge <72221999+bjurneiz@users.noreply.github.com>
2022-02-17 13:20:32 +0100

    Endret filene til php filer. Ikke endret på edit.html og edit.php siden de må merges

---
+++ settings.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="main.css" />
+    <title>Finance Budget App</title>
+    <style>
+    .switch {
+        position: relative;
+        display: inline-block;
+        width: 60px;
+        height: 34px;
+    }
+
+    .switch input { 
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+
+    .slider {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: lightgrey;
+        -webkit-transition: .4s;
+        transition: .4s;
+    }
+
+    .slider:before {
+        position: absolute;
+        content: "";
+        height: 26px;
+        width: 26px;
+        left: 4px;
+        bottom: 4px;
+        background-color: white;
+        -webkit-transition: .4s;
+        transition: .4s;
+    }
+
+    input:checked + .slider {
+        background-color: black;
+    }
+
+    input:focus + .slider {
+        box-shadow: 0 0 1px #2196F3;
+    }
+
+    input:checked + .slider:before {
+        -webkit-transform: translateX(26px);
+        -ms-transform: translateX(26px);
+        transform: translateX(26px);
+    }
+
+    /* Rounded sliders */
+    .slider.round {
+        border-radius: 34px;
+    }
+
+    .slider.round:before {
+        border-radius: 50%;
+    }
+
+
+
+    body {
+        padding: 25px;
+        background-color: white;
+        color: black;
+        font-size: 25px;
+    }
+
+    .dark-mode {
+        background-color: rgb(48, 48, 48);
+        color: white;
+    }
+    </style>
+  </head>
+  <body>
+    <header class="block" style="justify-content: left;">
+      <div id="sideMenu">
+        <div style="align-self: flex-start;">
+          <a href="home.html"
+            ><div class="block sideMenuItem">
+              <img
+                src="https://www.svgrepo.com/show/14443/home.svg"
+                class="sideMenuIcon"
+              />Home
+            </div></a
+          >
+          <a href="budget.html"
+            ><div class="block sideMenuItem">
+              <img
+                src="https://www.svgrepo.com/show/17167/pie-chart.svg"
+                class="sideMenuIcon"
+              />Budget
+            </div></a
+          >
+          <a href="budget-planner.html"
+            ><div class="block sideMenuItem">
+              <img
+                src="https://www.svgrepo.com/show/11983/from-a-to-z.svg"
+                class="sideMenuIcon"
+              />Budget planner
+            </div></a
+          >
+          <a href="achievements.html"
+            ><div class="block sideMenuItem">
+              <img
+                src="https://www.svgrepo.com/show/84275/trophy.svg"
+                class="sideMenuIcon"
+              />Achievements
+            </div></a
+          >
+        </div>
+        <div style="align-self: flex-end; margin-bottom: 40px;">
+          <a href="profile.html"
+            ><div class="block sideMenuItem">
+              <img
+                src="https://www.svgrepo.com/show/7025/user.svg"
+                class="sideMenuIcon"
+              />Profile
+            </div></a
+          >
+          <a href="settings.html"
+            ><div class="block sideMenuItem">
+              <img
+                src="https://www.svgrepo.com/show/198090/gear.svg"
+                class="sideMenuIcon"
+              />Settings
+            </div></a
+          >
+          <a href="faq.html"
+            ><div class="block sideMenuItem">
+              <img
+                src="https://www.svgrepo.com/show/348371/help.svg"
+                class="sideMenuIcon"
+              />Help
+            </div></a
+          >
+          <a href="index.html"
+            ><div class="block sideMenuItem">
+              <img
+                src="https://www.svgrepo.com/show/334066/log-out-circle.svg"
+                class="sideMenuIcon"
+              />Log out
+            </div></a
+          >
+        </div>
+      </div>
+
+      <img
+        id="menuClosed"
+        class="headerLogo"
+        src="https://www.svgrepo.com/show/336031/hamburger-button.svg"
+        style="position: fixed;"
+      />
+      <h1 style="margin-left: 100px;">Finance Budget App</h1>
+    </header>
+
+    <main>
+      <div class="block" style="">
+        <div class="contentBox" style="max-width: 1500px; width: 100%; margin: 50px 0;">
+          <h1>Settings</h1>
+        <p>Dark mode</p>
+        <label class="switch" id="toggleButton">
+            <input type="checkbox">
+            <span class="slider round"></span>
+        </label>
+
+        <button onclick="myFunction()">Toggle dark mode</button>
+
+        <script>
+            var toggleButton = document.getElementById("toggleButton");
+            toggleButton.onclick = myFunction();
+
+            function myFunction() {
+                var element = document.body;
+                element.classList.toggle("dark-mode");
+            }
+            
+        </script>
+        </div>
+      </div>
+    </main>
+      
+
+    <footer>
+      <ul>
+        <li>
+          <a href="home.html">Home</a>
+        </li>
+        <li>
+          <a href="faq.html">FAQ</a>
+        </li>
+        <li>
+          <a href="about.html">About</a>
+        </li>
+        <li>
+          <a href="contactForm/index.php">Contact</a>
+        </li>
+      </ul>
+      <p>&copy; 2021 Finance Budget App AS</p>
+    </footer>
+
+    <script src="main.js"></script>
+  </body>
+</html>
