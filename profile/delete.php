848a3cd642fa13f6996ed6300d96ce9c7c46f322
Bjørnar Borge <72221999+bjurneiz@users.noreply.github.com>
2022-02-24 14:30:27 +0100

    Det er nå mulig å slette bruker

--- profile/delete.php
+++ profile/delete.php
@@ -1,3 +1,35 @@
+<?php
+	session_start();
+	$email_old = $_SESSION['email'];
+
+	include '../connect_mysql/connect.php';
+	$conn = OpenCon();
+
+	$sql = "SELECT * FROM Customer WHERE EMail='$email_old'";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+	$id = $row['CustomerID'];
+
+    if(isset($_POST['delete_btn'])) {
+        $email=mysqli_real_escape_string($conn,$_POST['email']);
+        if($email_old == $email) {
+            try {
+                $sql =	"DELETE FROM Customer WHERE CustomerID = '$id'";
+                mysqli_query($conn,$sql); 
+                header("location:../index.php"); 
+                session_destroy();
+              } catch (Exception $e) {
+                echo '<script language="javascript">';
+                echo 'alert("An error occured. Contact us for help!")';
+                echo '</script>'; 
+              }
+        } else {
+            echo '<script language="javascript">';
+            echo 'alert("Wrong e-mail!")';
+            echo '</script>'; 
+        }
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,12 +50,11 @@
 
     <main>
       <div></div>
-      <form class="block" action="edit.php" method="post">
-
+      <form class="block" action="delete.php" method="post">
         <div class="contentBox">
           <label style="margin-top: 50px;">Confirm e-mail</label>
           <input type="text" name='email' required>
-          <a><button type="submit" class="loginButton" value="Edit" name="update_btn">Delete account</button></a>
+          <a><button type="submit" class="loginButton" value="Delete" name="delete_btn">Delete account</button></a>
           <p>
           </p>
           <h3 style="margin-top: 100px;"><a href="../profile.php">Cancel</a></h3>
