89456a0cfd2108ea4cb23b4e1ddca95565bc3a78
Bjørnar Borge <72221999+bjurneiz@users.noreply.github.com>
2022-03-02 20:08:44 +0100

    Update test.sql
    Oppdatert skriptet.

--- mysql_scripts/test.sql
+++ mysql_scripts/test.sql
@@ -1,46 +1,45 @@
-CREATE DATABASE financeBudgetApp;
+USE FinanceBudgetApp; -- Use the created database
 
-USE financeBudgetApp;
-
+-- Drop all the tables if exists before creating new
 DROP TABLE IF EXISTS Customer;
+DROP TABLE IF EXISTS Images;
+DROP TABLE IF EXISTS UserAchievement;
 
 CREATE TABLE Customer (
-CustomerID int NOT NULL,
-EMail varchar(100) NOT NULL,
-Password varchar(100) NOT NULL,
-phone varchar(15),
-home varchar(40),
-CONSTRAINT UC_Person UNIQUE (ID,LastName)
+  CustomerID int NOT NULL,
+  Email varchar(100) NOT NULL,
+  Password varchar(100) NOT NULL,
+  Phone varchar(15),
+  Home varchar(40),
+  PRIMARY KEY (CustomerID),
+  UNIQUE (Email)
 );
-ALTER TABLE Customer DROP PRIMARY KEY, ADD PRIMARY KEY (CustomerID, EMail ); 
 
-ALTER TABLE Customer
-ADD UNIQUE (EMail);
-
-SELECT *
-FROM Customer;
-
-DROP TABLE images;
-CREATE TABLE images (
+CREATE TABLE Images (
   CustomerID int NOT NULL,
   name varchar(200) NOT NULL,
-  CONSTRAINT imagesPK PRIMARY KEY (CustomerID),
-  CONSTRAINT imagesFK FOREIGN KEY(CustomerID) REFERENCES customer (CustomerID)
--- Achievement table
-CREATE TABLE achievement
-(
-AchievementID CHAR(10),
-AchievementName CHAR(50),
-AchievementDescr CHAR(50),
-CONSTRAINT achievementPK PRIMARY KEY(AchievementID)
-);
+  CONSTRAINT ImagesPK PRIMARY KEY (Customerid),
+  CONSTRAINT ImagesFK FOREIGN KEY(Customerid) REFERENCES Customer (CustomerID)
 
 -- UserAchievement table
-CREATE TABLE userachievement
-(
-AchievementID CHAR(10),
-CustomerID int,
-CONSTRAINT userachievementPK PRIMARY KEY(AchievementID,CustomerID),
-CONSTRAINT AchievementIDFK FOREIGN KEY(AchievementID) REFERENCES achievement(AchievementID),
-CONSTRAINT CustomerIDFK FOREIGN KEY(CustomerID) REFERENCES customer(CustomerID)
+CREATE TABLE UserAchievement (
+  AchievementID CHAR(10),
+  CustomerID int,
+  CONSTRAINT UserachievementPK PRIMARY KEY (AchievementID, CustomerID),
+  CONSTRAINT AchievementIDFK FOREIGN KEY (AchievementID) REFERENCES Achievement (AchievementID),
+  CONSTRAINT CustomerIDFK FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID)
+);
+
+
+
+
+
+-- Faste tabeller - ikke kjør drop på disse da innholdet er satt
+
+-- Achievement table
+CREATE TABLE Achievement (
+  AchievementID CHAR(10),
+  AchievementName CHAR(50),
+  AchievementDescr CHAR(50),
+  CONSTRAINT AchievementPK PRIMARY KEY (AchievementID)
 );
