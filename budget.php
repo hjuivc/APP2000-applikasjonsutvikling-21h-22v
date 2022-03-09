7ac2f5168ba30309c772dc60f9945ca92963e3aa
Eiriksm0raas <eirik.sm0raas@gmail.com>
2022-03-09 20:14:49 +0100

    Flytter graph funksjoner
    Flytter standard funskjoner for å oprette graphs inn i en egen script fil: graphs.js, slik at det blir enklere å benytte dem i overvire på home.php siden vår uten at vi må skrive dobbel kode

--- budget.php
+++ budget.php
@@ -184,63 +184,9 @@
 
     <script src="main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js"></script>
+    <script src="graphs.js"></script>
     <script>
-
-      const TYPE_BAR_HORISONTAL   = 0;
-      const TYPE_BAR_VERTICAL     = 1;
-      const TYPE_LINE             = 2;
-
-      Chart.defaults.font.family  = "sans-serif";
-      Chart.defaults.font.size    = 18;
-      Chart.defaults.color        = "#262220";
-
-      function createChart(canvas, names, data, type) {
-
-        var chartType, axis, showLegend;
-
-        switch(type) {
-
-          case TYPE_BAR_HORISONTAL:
-            chartType   = "bar";
-            axis        = 'y';
-            showLegend  = false;
-            break;
-
-          case TYPE_BAR_VERTICAL:
-            chartType   = "bar";
-            axis        = 'x';
-            showLegend  = false;
-            break;
-
-          case TYPE_LINE:
-            chartType   = "line";
-            axis        = 'x';
-            showLegend  = false;
-            break;
-        }
-
-        var myData = {
-          labels: names,
-          datasets: [{
-            data: data,
-            backgroundColor: "#262220"
-          }]
-        };
-
-        return new Chart(canvas, {
-          type: chartType,
-          data: myData,
-          options: {
-            indexAxis: axis,
-            plugins: {
-              legend: {
-                display: showLegend
-              }
-            }
-          }
-        });
-      }
-
+      
       // Data vi kommer til å hente fra databasen
       
       var incomeValues  = [];
---
+++ graphs.js
@@ -0,0 +1,54 @@
+const TYPE_BAR_HORISONTAL   = 0;
+const TYPE_BAR_VERTICAL     = 1;
+const TYPE_LINE             = 2;
+
+Chart.defaults.font.family  = "sans-serif";
+Chart.defaults.font.size    = 18;
+Chart.defaults.color        = "#262220";
+
+function createChart(canvas, names, data, type) {
+
+  var chartType, axis, showLegend;
+
+  switch(type) {
+
+    case TYPE_BAR_HORISONTAL:
+      chartType   = "bar";
+      axis        = 'y';
+      showLegend  = false;
+      break;
+
+    case TYPE_BAR_VERTICAL:
+      chartType   = "bar";
+      axis        = 'x';
+      showLegend  = false;
+      break;
+
+    case TYPE_LINE:
+      chartType   = "line";
+      axis        = 'x';
+      showLegend  = false;
+      break;
+  }
+
+  var myData = {
+    labels: names,
+    datasets: [{
+      data: data,
+      backgroundColor: "#262220"
+    }]
+  };
+
+  return new Chart(canvas, {
+    type: chartType,
+    data: myData,
+    options: {
+      indexAxis: axis,
+      plugins: {
+        legend: {
+          display: showLegend
+        }
+      }
+    }
+  });
+}
