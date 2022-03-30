ebc6e43bfae4753b992845279e766f5ea3ddde18
Bjørnar Borge <72221999+bjbogithub@users.noreply.github.com>
2022-03-30 21:08:12 +0200

    Update faqOffline.php

--- offlineSites/faqOffline.php
+++ offlineSites/faqOffline.php
@@ -13,45 +13,36 @@
       </header>
 
       <main>
-        <h1 style="text-align: center;">Help / FAQ</h1>
-        <div class="block">
-          <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 10px 0;">
-            <h2>Question</h2>
-            <p>Q: How to change password</p>
-            <p>A: Click on <a href="forgot-passwordOffline.php">"forgot password"</a> and follow the steps from the recieved mail.</p>
-          </div>
-        </div>
-      </main>
-
-      <main>
-        <div class="block">
-          <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 10px 0;">
-            <h2>Question</h2>
-            <p>Q: How to get achievements</p>
-            <p>A: Explore the application to unlock new achievements</p>
-          </div>
-        </div>
-      </main>
-
-      <main>
-          <div class="block">
-          <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 10px 0;">
-            <h2>Question</h2>
-            <p>Q</p>
-            <p>A</p>
-          </div>
-        </div>
-      </main>
-
-      <main>
-          <div class="block">
-          <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 10px 0;">
-            <h2>Question</h2>
-            <p>Q</p>
-            <p>A</p>
-          </div>
-        </div>
-      </main>
+            <h1 style="text-align: center;">Help / FAQ</h1>
+            <div class="block"style="width: 90%; margin-left:auto; margin-right:auto;">
+                <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 30px 0;">
+                    <h2>Getting started on Finance Budget App</h2>
+                    <p>Navigate to start page and press register button. On the next page you have to enter your credentials and press register.</p>
+                    <p>A: Click on <a href="forgot-password.php">"forgot password"</a> and follow the steps from the recieved mail.</p>
+                </div>
+            </div>
+            <div class="block" style="width: 90%; margin-left:auto; margin-right:auto;">
+            <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 30px 0;">
+                <h2>Question</h2>
+                <p>Q: Curriculum Vitae</p>
+                <p>A: Congratulation</p>
+            </div>
+            </div>
+            <div class="block" style="width: 90%; margin-left:auto; margin-right:auto;">
+            <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 30px 0;">
+                <h2>Question</h2>
+                <p>Q</p>
+                <p>A</p>
+            </div>
+            </div>
+            <div class="block" style="width: 90%; margin-left:auto; margin-right:auto;">
+                <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 30px 0;">
+                    <h2>Question</h2>
+                    <p>Q</p>
+                    <p>A</p>
+                </div>
+            </div>
+        </main>
 
       <footer>
         <ul>
