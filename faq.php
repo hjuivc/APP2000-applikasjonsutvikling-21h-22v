02aaf9c51fccdaf951c2b5bc10fd2cb47cf7abb7
aass90 <139959@usn.no>
2022-03-31 10:07:48 +0200

    Update faq.php
    la til bittelitt informasjon

--- faq.php
+++ faq.php
@@ -123,23 +123,23 @@
             </div>
             <div class="block" style="width: 90%; margin-left:auto; margin-right:auto;">
             <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 30px 0;">
-                <h2>Question</h2>
-                <p>Q: Curriculum Vitae</p>
-                <p>A: Congratulation</p>
+                <h2>Setting up your first budget</h2>
+                <p>Q: How do I add saving goals?</p>
+                <p>A: Navigate to Budget Planner and start with naming your saving goal, then add your desired amount.</p>
             </div>
             </div>
             <div class="block" style="width: 90%; margin-left:auto; margin-right:auto;">
             <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 30px 0;">
-                <h2>Question</h2>
-                <p>Q</p>
-                <p>A</p>
+                <h2>How to see my summary for my budget</h2>
+                <p>On Home, how to see what I am saving for, and how far am I?</p>
+                <p>In Budget Planner you will have to hit the "Update" button for the app to calculate and add your summary to the Home- page.</p>
             </div>
             </div>
             <div class="block" style="width: 90%; margin-left:auto; margin-right:auto;">
                 <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 30px 0;">
-                    <h2>Question</h2>
-                    <p>Q</p>
-                    <p>A</p>
+                    <h2>Edit profile- information</h2>
+                    <p>I have a new email- address</p>
+                    <p>Simply navigate to your profile page, then hit "Edit profile"</p>
                 </div>
             </div>
         </main>
