611bce479daea556fdee5f5a153c7f86259b0fce
Bjørnar Borge <72221999+arcanumursi@users.noreply.github.com>
2022-04-05 11:44:12 +0200

    Update forgot.php

--- forgot-password/forgot.php
+++ forgot-password/forgot.php
@@ -14,7 +14,7 @@ if(isset($_POST["forgot-submit"])) {
 	$token		= random_bytes(32);
 
 	$link		= 
-		"localhost/forgot-password/create-new-password.php?selector=" 
+		"localhost/APP2000-applikasjonsutvikling-21h-22v/forgot-password/create-new-password.php?selector=" 
 		. $selector .
 		"&validator=" 
 		. bin2hex($token);
