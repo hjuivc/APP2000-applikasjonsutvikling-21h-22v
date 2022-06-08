6571d77a255896d4525bb4780a47542241724fad
Eiriksm0raas <eirik.sm0raas@gmail.com>
2022-06-08 18:27:08 +0200

    Fikset bug
    Fikset en bug hvor om du hadde mer enn 100% progressjon på et saving goal, så ville baren fortsatte å gå lengre og lengre til høyre

--- progress.js
+++ progress.js
@@ -7,7 +7,7 @@ function progressBar(id, labelID, title, percent, total , color1 = "#f7f1f0", co
 			"<div style='width:80%; height: 50px; background-color: " 
 		+ 	color1 + "; margin: auto; border-radius: 3.5px;'>" 
 		+ 	"<div style='width:" 
-		+ 	percent + "%; background-color: " 
+		+ 	Math.min(percent, 100) + "%; background-color: " 
 		+ 	color2 + "; height: 100%; border-radius: "
 		+	(percent == 100? "3.5" : "3.5px 0 0 3.5")
 		+ 	"px'></div>"
