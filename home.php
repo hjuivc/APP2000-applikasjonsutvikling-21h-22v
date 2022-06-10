b2e26a90612011f7ce795d3a5b2bbe850fe04e18
Bjørnar Borge <72221999+arcanumursi@users.noreply.github.com>
2022-06-10 21:19:42 +0200

    Update home.php

--- home.php
+++ home.php
@@ -372,7 +372,7 @@ if (mysqli_num_rows($rowsql1) > 0) {
         }
       </script>
       <div class="block" style="width: 90%; margin-left:auto; margin-right:auto;">
-        <div class="contentBox" style="max-width: 1500px; width: 100%; margin: 50px 0;">
+        <div class="contentBox" style="max-width: 1500px; width: 100%; margin: 20px 20px;">
           <h2>Overview</h2>
           <div style="width: 50%; float: left;">
             <h2 id="progressLabel" style="text-align: center;">Saving goal</h2>
@@ -386,12 +386,12 @@ if (mysqli_num_rows($rowsql1) > 0) {
         </div>
       </div>
       <div class="block" style="width: 90%; margin-left:auto; margin-right:auto;">
-        <div class="contentBox" style="max-width: 725px; width: 100%; margin: 0; margin-right: 50px; padding-bottom: 40px;">
+        <div class="contentBox" style="max-width: 725px; width: 100%;  margin: 20px 20px; padding-bottom: 40px;">
           <h2>Budget</h2>
           <canvas id="budgetCanvas"></canvas>
         </div>
             
-        <div class="contentBox" style="max-width: 725px; width: 100%; margin: 0;">
+        <div class="contentBox" style="max-width: 725px; width: 100%;  margin: 20px 20px;">
           <h2 style="text-align: center;">Achievements</h2>
           
           <div style="margin: 10px;">
