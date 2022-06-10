09b7e01567d3197ac47c60a96fb8ba1078957c76
Bjørnar Borge <72221999+arcanumursi@users.noreply.github.com>
2022-06-10 21:19:40 +0200

    Update faq.php

--- faq.php
+++ faq.php
@@ -116,28 +116,28 @@
         <main>
             <h1 style="text-align: center;">Help / FAQ</h1>
             <div class="block"style="width: 90%; margin-left:auto; margin-right:auto;">
-                <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 30px 0;">
+                <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 20px 30px;">
                     <h2>Lost my password</h2>
                     <p>Q: Lost my password, what to do?</p>
                     <p>A: Click on <a href="forgot-password.php">"forgot password"</a> and follow the steps from the recieved mail.</p>
                 </div>
             </div>
             <div class="block" style="width: 90%; margin-left:auto; margin-right:auto;">
-            <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 30px 0;">
+            <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 20px 30px;">
                 <h2>Setting up your first budget</h2>
                 <p>Q: How do I add saving goals?</p>
                 <p>A: Navigate to Budget Planner and start with naming your saving goal, then add your desired amount.</p>
             </div>
             </div>
             <div class="block" style="width: 90%; margin-left:auto; margin-right:auto;">
-            <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 30px 0;">
+            <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 20px 30px;">
                 <h2>How to see my summary for my budget</h2>
                 <p>Q: On Home, how to see what I am saving for, and how far am I?</p>
                 <p>A: In Budget Planner you will have to hit the "Update" button for the app to calculate and add your summary to the Home- page.</p>
             </div>
             </div>
             <div class="block" style="width: 90%; margin-left:auto; margin-right:auto;">
-                <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 30px 0;">
+                <div class="contentBoxFAQ" style="max-width: 1500px; width: 100%; margin: 20px 30px;">
                     <h2>Edit profile- information</h2>
                     <p>Q: I have a new email- address</p>
                     <p>A: Simply navigate to your profile page, then hit "Edit profile"</p>
